a6bbd44c87ec4fc6c606322ee285c4da7b83f0e3
thewoowon <thewoowon@naver.com>
2024-05-29 01:55:05 +0900

    refactor: postgresql leak 제거

--- app/db/base.py
+++ app/db/base.py
@@ -1,9 +1,3 @@
-# Host: pg-ffp3k.vpc-pub-cdb-kr.ntruss.com
-# Port: 5432
-# User: postgre
-# PW: planetnerd1!
-# DB: postgres
-
 # db/base.py
 from sqlalchemy.ext.declarative import declarative_base
 
--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-DATABASE_URL = "postgresql+asyncpg://postgre:planetnerd1!@pg-ffp3k.vpc-pub-cdb-kr.ntruss.com:5432/postgres"
+DATABASE_URL = ""
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
